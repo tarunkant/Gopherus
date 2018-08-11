94e18bba2d3056f90c6e00caebc1719b651916d6
tarunkant <tarunkant05@gmail.com>
2018-08-12 03:17:25 +0530

    Added Install file

---
+++ gopherus.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python2
+import argparse
+import sys
+sys.path.insert(0,'./scripts/')
+from scripts import FastCGI, MySQL, DumpMemcached, PHPMemcached, PyMemcached, RbMemcached, Redis, SMTP, Zabbix
---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+pip install argparse
+pip install requests
+chmod +x gopherus.py
+ln -sf $(pwd)/gopherus.py /usr/local/bin/gopherus
